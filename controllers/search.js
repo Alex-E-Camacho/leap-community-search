b4f9f75f5d73b1392a5a4e857db29251b7b08a59
Alex Camacho <alex.e.camacho@gmail.com>
2018-04-11 11:27:13 -0700

    Connect to simple stackoverflow api endpoint, display results in results.ejs

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 let bing = require('../helpers/bingSearch.js');
+const stackoverflow_search = require('../helpers/stackoverflowSearch.js');
 
 router.get('/search', function(req, res) {
     res.render('../views/index');
@@ -10,11 +11,14 @@ router.get('/search/results', async function(req, res) {
     let searchResults = bing.bing_web_search(req.query.search_query);
     let test = await searchResults.then(function(results) {
         return results;
-        //console.log(results); <--This shows objects just fine
     });
-    console.log(test); //This returns Promise { pending }
+    let so_search = stackoverflow_search.stackoverflow_search(req.query.search_query)
+    let soSearchResults = await so_search.then(function(results) {
+        return results;
+    })
     res.render('../views/results', {
-        test : test
+        test : test,
+        soSearchResults : soSearchResults
     });
 })
 

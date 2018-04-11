151c8777d5879cae479c511f88a7ac6c27e7e213
Alex Camacho <alex.e.camacho@gmail.com>
2018-04-10 20:43:22 -0700

    Use async/await to unwrap promise, iterate through JSON object in view

--- controllers/search.js
+++ controllers/search.js
@@ -6,11 +6,13 @@ router.get('/search', function(req, res) {
     res.render('../views/index');
 });
 
-router.get('/search/results', function(req, res) {
+router.get('/search/results', async function(req, res) {
     let searchResults = bing.bing_web_search(req.query.search_query);
-    let test = searchResults.then(function(results) {
-         return results;
+    let test = await searchResults.then(function(results) {
+        return results;
+        //console.log(results); <--This shows objects just fine
     });
+    console.log(test); //This returns Promise { pending }
     res.render('../views/results', {
         test : test
     });

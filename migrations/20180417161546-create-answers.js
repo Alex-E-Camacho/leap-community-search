151d6bf73ae2da6c6df9e730df94f9dda990407b
Alex Camacho <alex.e.camacho@gmail.com>
2018-04-17 09:41:03 -0700

    Add Answers attributes to migration

--- migrations/20180417161546-create-answers.js
+++ migrations/20180417161546-create-answers.js
@@ -2,22 +2,37 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('Answers', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      url: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      source: {
+        type: Sequelize.TEXT
+      },
+      external: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      search_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: Search,
+          key: 'id'
+        }
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+      return queryInterface.dropTable('answers');
   }
 };
